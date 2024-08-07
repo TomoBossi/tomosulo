3b29143bc455551aeb8fa0a7db5c7b515ac6e287
TomoBossi <tomasbossi97@gmail.com>
2024-08-06 23:07:55 -0300

    Fix missing tags, add inner table text, enable loading instructions and resetting state

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+p5.disableFriendlyErrors = true;
+
 const numRegisters = 16;
 const numInstructions = 8;
 const maxValue = 65535; // 0xFFFF
@@ -8,6 +10,8 @@ const tableLabelColor = 100;
 const tableStrokeColor = 100;
 const cdbStrokeColor = 100;
 const titleColor = 150;
+const tableTextColor = 150;
+const tableTextDefaultColor = 50;
 
 let s;
 
@@ -38,6 +42,8 @@ let rsALU;
 
 let memory = [];
 
+let instructionLoadIndex = 0;
+
 let resetButtonPos = {x: 0, y: 0};
 let loadButtonPos = {x: 0, y: 0};
 let stepButtonPos = {x: 0, y: 0};
@@ -46,15 +52,16 @@ function setup() {
   textFont('Courier New');
   smooth();
   
-  s = min(windowWidth/560, 5); // Eyeballed
+  s = min(windowWidth/525, 5); // Eyeballed
 
-  tableOuterStrokeWidth = 1*s;
-  tableInnerStrokeWidth = 1*s/3;
+  tableOuterStrokeWidth = s;
+  tableInnerStrokeWidth = s;
+  tableInnerStrokeOpacity = 0.35;
   rowHeight = 10*s;
   labelTextSize = 5*s;
   rowLabelTextShift = -2*s;
   tableSeparation = 25*s;
-  tablePaddingLeft = 15*s;
+  tablePaddingLeft = 12.5*s;
   tablePaddingTop = 15*s;
   cdbY = 2*tablePaddingTop + numRegisters*rowHeight;
   titleTextSize = 18.5*s;
@@ -86,8 +93,8 @@ function setup() {
   inputField.style.overflow = 'hidden';
   inputField.style.fontFamily = 'monospace';
   inputField.style.zindex = 10;
-  inputField.placeholder = 'MOV R1, 0xB0CA\nMOV R2, 0xB0CA\nADD R3, R1, R2'
-  inputField.style.backgroundColor = `rgb(${tableBgColor},${tableBgColor},${tableBgColor})`;
+  inputField.placeholder = 'MOV R1, 0xB0CA\nMOV R2, 0xB0CA\nADD R3, R1, R2';
+  inputField.style.backgroundColor = 'transparent'; // `rgb(${tableBgColor},${tableBgColor},${tableBgColor})`;
   inputField.style.color = `rgb(${titleColor},${titleColor},${titleColor})`;
   inputField.style.border = 'none'; // `${tableOuterStrokeWidth}px solid`;
   inputField.style.borderWidth = `${tableOuterStrokeWidth}px`;
@@ -96,13 +103,15 @@ function setup() {
   inputField.style.padding = 0.1*s;
   inputField.style.fontSize = `${4.8*s}px`;
 
-  inputField.addEventListener('input', function () {
-    const lines = this.value.split('\n').map(line => line.substring(0, 'INST [RXX], [0xXXXX], [0xXXXX]'.length));
-    this.value = lines.slice(0, numInstructions).join('\n');
-    if (lines.length > numInstructions) {
-      this.selectionStart = this.selectionEnd = this.value.length;
+  inputField.addEventListener('input', 
+    () => {
+      const lines = inputField.value.split('\n').map(line => line.substring(0, 'INST [RXX], [0xXXXX], [0xXXXX]'.length));
+      inputField.value = lines.slice(0, numInstructions).join('\n');
+      if (lines.length > numInstructions) {
+        inputField.selectionStart = inputField.selectionEnd = inputField.value.length;
+      }
     }
-  });
+  );
 
   resetButtonPos = {x: tablePaddingLeft + tableSeparation + inst.width, y: tablePaddingTop};
   loadButtonPos = {x: resetButtonPos.x, y: tablePaddingTop + 2*rowHeight};
@@ -123,11 +132,13 @@ function draw() {
   drawTable(inst);
   drawTable(rsALU);
   drawTable(rsLSU);
+  drawText();
 }
 
 function drawButton(x, y, label, connectLeft = true) {
   if (connectLeft) {
     setConfigTableRows(tableInnerStrokeWidth);
+    stroke(tableStrokeColor, 255*tableInnerStrokeOpacity);
     line(x - tableSeparation, y + rowHeight/2, x, y + rowHeight/2);
   }
   setConfigTableRows(tableOuterStrokeWidth);
@@ -182,11 +193,12 @@ function drawTable(table, tag = false, init = false) {
       table.values.push({...table.initValue}); // Shallow copy
     }
     if (table.rowLabels.length > i) {
-      text(table.rowLabels[i], table.pos.x+rowLabelTextShift, table.pos.y+(i+0.5)*rowHeight);
+      text(table.rowLabels[i], table.pos.x+rowLabelTextShift, table.pos.y+(i+0.5)*rowHeight + 0.5*s);
     }
   }
   
   strokeWeight(tableInnerStrokeWidth);
+  stroke(tableStrokeColor, 255*tableInnerStrokeOpacity);
   for (let x of table.divs) {
     line(table.pos.x+x, table.pos.y, table.pos.x+x, table.pos.y+rowHeight*table.rows);
   }
@@ -206,13 +218,14 @@ function drawTable(table, tag = false, init = false) {
     setConfigTableRows(tableOuterStrokeWidth);
     quad(
       table.pos.x+1.5*s, table.pos.y + table.rows*rowHeight,
-      table.pos.x+table.width/2-25*s, table.pos.y + (table.rows+1)*rowHeight,
-      table.pos.x+table.width/2+25*s, table.pos.y + (table.rows+1)*rowHeight,
+      table.pos.x+table.width/2-(25-8.75)*s, table.pos.y + (table.rows+1)*rowHeight,
+      table.pos.x+table.width/2+(25-8.75)*s, table.pos.y + (table.rows+1)*rowHeight,
       table.pos.x-1.5*s+table.width, table.pos.y + table.rows*rowHeight
     );
-    rect(table.pos.x+table.width/2-25*s, table.pos.y+(table.rows+1)*rowHeight, 50*s, rowHeight);
+    rect(table.pos.x+table.width/2-(25-8.75)*s, table.pos.y+(table.rows+1)*rowHeight, 32.5*s, rowHeight);
     strokeWeight(tableInnerStrokeWidth);
-    line(table.pos.x+table.width/2-25*s+10*s, table.pos.y+(table.rows+1)*rowHeight, table.pos.x+table.width/2-25*s+10*s, table.pos.y+(table.rows+2)*rowHeight);
+    stroke(tableStrokeColor, 255*tableInnerStrokeOpacity);
+    line(table.pos.x+table.width/2-(25-8.75)*s+10*s, table.pos.y+(table.rows+1)*rowHeight, table.pos.x+table.width/2-(25-8.75)*s+10*s, table.pos.y+(table.rows+2)*rowHeight);
     setConfigLabels(labelTextSize, 'center');
     text(table.outputLabel, table.pos.x+table.width/2, table.pos.y+(table.rows+0.5)*rowHeight);
   }
@@ -258,14 +271,14 @@ function initTables() {
     {
       pos: {x: tablePaddingLeft + tableSeparation + inst.width, y: tablePaddingTop + rowHeight*(numRegisters-numInstructions)},
       rows: numInstructions-2,
-      width: 120*s,
-      divs: [20*s, 60*s, 70*s, 110*s],
-      columnLabels: ['OpCode', 'Operand 1', 'V', 'Operand 2', 'V'],
+      width: 105*s,
+      divs: [20*s, 30*s, 52.5*s, 62.5*s, 72.5*s, 95*s],
+      columnLabels: ['Code', 'Tag', 'Op.1', 'V', 'Tag', 'Op.2', 'V'],
       rowLabels: [],
-      initValue: {opcode: '', operands: [{value: 0, v: 0}, {value: 0, v: 0}]},
+      initValue: {opcode: '', operands: [{tag: '~', value: 0, v: 0}, {tag: '~', value: 0, v: 0}]},
       values: [],
       outputValue: {tag: '', value: 0},
-      outputLabel: 'Arithmetic-Logic Unit'
+      outputLabel: 'ALU' // 'Arithmetic-Logic Unit'
     }, true, true
   );
   
@@ -273,31 +286,16 @@ function initTables() {
     {
       pos: {x: tablePaddingLeft + 2*tableSeparation + inst.width + rsALU.width, y: tablePaddingTop + rowHeight*(numRegisters-numInstructions)},
       rows: numInstructions-2,
-      width: 120*s,
-      divs: [20*s, 60*s, 70*s, 110*s],
-      columnLabels: ['OpCode', 'Operand 1', 'V', 'Operand 2', 'V'],
+      width: 105*s,
+      divs: [20*s, 30*s, 52.5*s, 62.5*s, 72.5*s, 95*s],
+      columnLabels: ['Code', 'Tag', 'Op.1', 'V', 'Tag', 'Op.2', 'V'],
       rowLabels: [],
-      initValue: {opcode: '', operands: [{value: 0, v: 0}, {value: 0, v: 0}]},
+      initValue: {opcode: '', operands: [{tag: '~', value: 0, v: 0}, {tag: '~', value: 0, v: 0}]},
       values: [],
       outputValue: {tag: '', value: 0},
-      outputLabel: 'Load-Store Unit'
+      outputLabel: 'LSU' // 'Load-Store Unit'
     }, true, true
   );
-  
-  // rsLSU = drawTable(
-  //   {
-  //     pos: {},
-  //     rows: numInstructions,
-  //     width: 70*s,
-  //     divs: [20*s, 60*s],
-  //     columnLabels: ['OpCode', 'Operand', 'V'],
-  //     rowLabels: [],
-  //     initValue: {opcode: '', operands: [{operand: 0, v: 0}]},
-  //     values: [],
-  //     outputValue: {},
-  //     outputLabel: ''
-  //   }, true, true
-  // );
 
   RAT = drawTable(
     {
@@ -333,11 +331,24 @@ function initTables() {
 function reset() {
   rsTag = 'A';
   initTables();
-  inputField.value = '';
+  instructionLoadIndex = 0;
+  // inputField.value = '';
 }
 
 function load() {
-  console.log('TODO');
+  let loaded = instructionLoadIndex;
+  let value = inputField.value;
+  if (value === '') {
+    value = inputField.placeholder;
+  }
+  for (let [i, instruction] of value.split('\n').map(line => line.toUpperCase()).entries()) {
+    if (instructionLoadIndex < numInstructions) {
+      inst.values[instructionLoadIndex].instruction = instruction;
+      instructionLoadIndex++;
+    }
+  }
+  loaded = instructionLoadIndex - loaded;
+  inputField.value = value.split('\n').slice(loaded).join('\n');
 }
 
 function step() {
@@ -351,7 +362,7 @@ function mouseHover(pos, w, h) {
 function mouseClicked() {
   if (mouseHover(resetButtonPos, buttonWidth, rowHeight)) {
     reset();
-  } else if (mouseHover(loadButtonPos, buttonWidth, rowHeight)) {
+  } else if (instructionLoadIndex < numInstructions && mouseHover(loadButtonPos, buttonWidth, rowHeight)) {
     load();
   }  else if (mouseHover(stepButtonPos, buttonWidth, rowHeight)) {
     step();
@@ -372,6 +383,10 @@ function mul(v1, v2) {
   return constrain(v1 * v2, 0, maxValue);
 }
 
+function mov(v1, v2) {
+  // TODO ???
+}
+
 function st(v1, v2) {
   memory[v1] = v2;
 }
@@ -381,4 +396,79 @@ function ld(v1) {
     memory[v1] = floor(random(0, maxValue/8));
   }
   return memory[v1];
+}
+
+// Write data
+
+function drawText() {
+  drawTextRs(rsALU);
+  drawTextRs(rsLSU);
+  drawTextRAT();
+  drawTextReg();
+  drawTextInst();
+}
+
+function setConfigTableText() {
+  strokeWeight(0);
+  textAlign('left', 'center');
+  textSize(labelTextSize);
+}
+
+function int2hex(value) {
+  return '0x'+value.toString(16).padStart(4, '0');
+}
+
+function drawTextRs(rs) {
+  setConfigTableText();
+  for (let [i, value] of rs.values.entries()) {
+    fill(tableTextColor);
+    if (deepEqual(value, rs.initValue)) {
+      fill(tableTextDefaultColor);
+    }
+    text(value.opcode, rs.pos.x + 2*s, rs.pos.y + (i+0.5)*rowHeight + 0.5*s);
+    text(value.operands[0].tag, rs.pos.x + rs.divs[0] + 3.5*s, rs.pos.y + (i+0.5)*rowHeight + 0.5*s);
+    text(int2hex(value.operands[0].value), rs.pos.x + rs.divs[1] + 2*s, rs.pos.y + (i+0.5)*rowHeight + 0.5*s);
+    text(value.operands[0].v, rs.pos.x + rs.divs[2] + 3.5*s, rs.pos.y + (i+0.5)*rowHeight + 0.5*s);
+    text(value.operands[1].tag, rs.pos.x + rs.divs[3] + 3.5*s, rs.pos.y + (i+0.5)*rowHeight + 0.5*s);
+    text(int2hex(value.operands[1].value), rs.pos.x + rs.divs[4] + 2*s, rs.pos.y + (i+0.5)*rowHeight + 0.5*s);
+    text(value.operands[1].v, rs.pos.x + rs.divs[5] + 3.5*s, rs.pos.y + (i+0.5)*rowHeight + 0.5*s);
+  }
+}
+
+function drawTextRAT() {
+  setConfigTableText();
+  for (let [i, value] of RAT.values.entries()) {
+    fill(tableTextColor);
+    if (deepEqual(value, RAT.initValue)) {
+      fill(tableTextDefaultColor);
+    }
+    text(value.tag, RAT.pos.x + 3.5*s, RAT.pos.y + (i+0.5)*rowHeight + 0.5*s);
+    text(int2hex(value.value), RAT.pos.x + RAT.divs[0] + 2*s, RAT.pos.y + (i+0.5)*rowHeight + 0.5*s);
+    text(value.v, RAT.pos.x + RAT.divs[1] + 3.5*s, RAT.pos.y + (i+0.5)*rowHeight + 0.5*s);
+  }
+}
+
+function drawTextReg() {
+  setConfigTableText();
+  fill(tableTextColor);
+  for (let [i, value] of reg.values.entries()) {
+    text(int2hex(value.value), reg.pos.x + 2*s, reg.pos.y + (i+0.5)*rowHeight + 0.5*s);
+  }
+}
+
+function drawTextInst() {
+  setConfigTableText();
+  fill(tableTextColor);
+  for (let [i, value] of inst.values.entries()) {
+    text(value.instruction, inst.pos.x + 2*s, inst.pos.y + numInstructions*rowHeight - (i+0.5)*rowHeight + 0.5*s);
+  }
+}
+
+// https://stackoverflow.com/a/32922084
+function deepEqual(x, y) {
+  const ok = Object.keys, tx = typeof x, ty = typeof y;
+  return x && y && tx === 'object' && tx === ty ? (
+    ok(x).length === ok(y).length &&
+      ok(x).every(key => deepEqual(x[key], y[key]))
+  ) : (x === y);
 }
